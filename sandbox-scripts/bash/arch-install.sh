3ab790979b7fdc5ac0b05a28f9b696d20b327457
JozeFe <jozefkrchoml@gmail.com>
2018-11-14 21:34:43 +0100

    add, arch install (not complete)

---
+++ sandbox-scripts/bash/arch-install.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# verify boot mode
+ls /sys/firmware/efi/efivars
+
+# update system clock
+timedatectl set-ntp true
+
+# disk
+lsblk
+fdisk -l
+
+mkfs.ext4 /dev/sdax # boot, root, home
+
+# swap
+mkswap /dev/sdax
+swapon /dev/sdax
+
+# mount
+mount /dev/sdax # /mnt/boot, /mnt, /mnt/home
+
+# wifi - iw
+iw dev # get name
+iw dev interface link # status
+
+ip link set interface up # activate interface
+ip link show interface # check <...,UP> value
+
+iw dev interface scan | grep -A 30 your_network # discover access point
+iw dev interface set type ibss # Set operating mode
+
+# Connect to an access point
+iw dev interface connect "your_essid" key 0:your_key
+iw dev interface connect "your_essid" key d:2:your_key
+
+# wpa - connect
+wpa_supplicant -B -i interface -c <(wpa_passphrase ssid key)
+dhcpcd
+
+# installation
+pacstrap /mnt base base-devel
