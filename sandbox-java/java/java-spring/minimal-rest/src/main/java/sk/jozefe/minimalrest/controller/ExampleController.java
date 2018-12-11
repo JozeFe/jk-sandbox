f85ade768e01deb5da8a5d914ee2a006669b319c
JozeFe <jozefkrchoml@gmail.com>
2018-12-11 17:01:27 +0100

    add sleep endpoint for minimal-rest, and docker build for minimal-rest

--- sandbox-java/java/java-spring/minimal-rest/src/main/java/sk/jozefe/minimalrest/controller/ExampleController.java
+++ sandbox-java/java/java-spring/minimal-rest/src/main/java/sk/jozefe/minimalrest/controller/ExampleController.java
@@ -1,6 +1,7 @@
 package sk.jozefe.minimalrest.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.jni.Time;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -39,5 +40,22 @@ public class ExampleController {
     public VariableDto variableGet(@PathVariable("variable") String variable) {
         LOG.debug("Processing passed variable");
         return exampleService.processVariable(variable);
+    }
+
+    @CrossOrigin
+    @GetMapping(path ="/sleep/{time}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public String sleep(@PathVariable("time") String time) {
+        LOG.debug("Processing sleep request for: {} sec", time);
+        try {
+            long duration = Long.parseLong(time);
+            Thread.sleep(duration * 1000);
+        } catch (NumberFormatException e) {
+            LOG.error("Fail to format variable {} into long", time);
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            LOG.error("Interrupted while Thread.sleep");
+            e.printStackTrace();
+        }
+        return "{\"message\":\"get-from-sleep\"}";
     }
 }
